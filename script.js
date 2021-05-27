fc7204e740bba5ca40125aecd367a7174acd5c07
B. Klara Simon <borbala.klara@gmail.com>
2021-05-27 12:15:13 +0200

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    title: "Attend webinar",
+    body: "Coaching webinar in June",
+  },
+  {
+    title: "Buy special spieces",
+    body: "Recreate the amazing curry recipe",
+  },
+  {
+    title: "Book a table for 4",
+    body: "Have a great time with my parents, in my favorite coffee shop",
+  },
+  {
+    title: "submit the coaching recording",
+    body: "review the transciption again",
+  },
+];
+
+// Filter
+
+const filter = {};
