d5276a7f9a19f2c5f52d85a1d49af927023c6972
B. Klara Simon <borbala.klara@gmail.com>
2021-05-27 13:07:37 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -19,4 +19,32 @@ const notes = [
 
 // Filter
 
-const filter = {};
+const filters = {
+  searchText: "",
+};
+
+// limit the notes only to filtered ones
+const renderNotes = function (notes, filters) {
+  const filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+  console.log(filteredNotes);
+
+  // this render on screen and push to html div container
+  document.querySelector("#notes").innerHTML = "";
+
+  // majd adja hozza az elementekt
+
+  filteredNotes.forEach(function (note) {
+    const noteElement = document.createElement("p");
+    noteElement.textContent = note.title;
+    document.querySelector("#notes").appendChild(noteElement);
+  });
+};
+
+renderNotes(notes, filters);
+
+document.querySelector("#search-text").addEventListener("input", function (e) {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
